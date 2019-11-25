7c170302f1f172b93d88c0b9d74ac7c355554f04
nickjo <nickjo180@gmail.com>
2019-11-25 13:33:40 +0900

    first

---
+++ ch01/1.opbasic.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Nov 25 11:35:23 2019
+
+@author: edu
+"""
+
+# 정수형의 Max 값?
+a = 12345678901234567890
+b = 10.12
+
+print(a + b)
+
+print('abc',"def")
+print('abc'+"def")
+
+print('abc',b)
